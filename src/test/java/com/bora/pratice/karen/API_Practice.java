677ecc26cdcfe2d8ff13cff025484b797d24cf4d
Karen Yang <yangxiaotong0412@gmail.com>
2021-01-29 00:47:56 -0500

    karen API test practice

--- src/test/java/com/bora/pratice/karen/API_Practice.java
+++ src/test/java/com/bora/pratice/karen/API_Practice.java
@@ -5,33 +5,35 @@ import java.util.HashMap;
 import org.json.simple.JSONObject;
 
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 public class API_Practice {
 
 	public static void main(String[] args) {
-		
-		RestAssured.baseURI="https://boratech.herokuapp.com";
-		String endpoint="/api/auth";
+
+		RestAssured.baseURI = "https://boratech.herokuapp.com";
+		String endpoint = "/api/auth";
 		RequestSpecification request = RestAssured.given();
 		request.header("Content-Type", "application/json");
-		HashMap<String, String> mapBody= new HashMap<String , String>();
-		
-		mapBody.put("email", "271866163kk@gmail.com");
-		mapBody.put("password", "Iliveindc0412");
+		HashMap<String, String> mapBody = new HashMap<String, String>();
 
-		JSONObject body= new JSONObject(mapBody);
+		mapBody.put("email", "myemail@gmail.com");
+		mapBody.put("password", "boratech123");
+
+		JSONObject body = new JSONObject(mapBody);
 		request.body(body);
-		
+
 		Response response = request.post(endpoint);
-		
+
 		int statusCode = response.getStatusCode();
-		System.out.println("Rsponse Status Code :"+ statusCode);
-		System.out.println("Respose  Body :"+ response.getBody().asString());
-		
-		
-		
+		System.out.println("Rsponse Status Code :" + statusCode);
+
+		JsonPath jasonpath = response.jsonPath();
+		String token = jasonpath.get("token");
+		System.out.println("the token :" + token);
+
 	}
 
 }
---
+++ src/test/java/com/bora/pratice/karen/BoraTechProfileInfo.java
@@ -0,0 +1,10 @@
+package com.bora.pratice.karen;
+
+public class BoraTechProfileInfo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
