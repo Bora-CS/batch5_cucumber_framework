46596aafecc2bdbfbab8744cd89b802304089de9
Anna Chen <xiangchen0704@gmail.com>
2021-02-13 13:09:05 -0500

    test

--- src/test/java/com/bora/utilities/BoraKEeyword_library.java
+++ src/test/java/com/bora/utilities/BoraKEeyword_library.java
@@ -1,5 +1,27 @@
 package com.bora.utilities;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import com.google.inject.spi.Element;
+
 public class BoraKEeyword_library {
+	private WebDriver driver;
+
+	public void login(String userName,String password) {
+		
+		 elementSendKey(By.name("email"),userName);
+		 elementSendKey(By.name("password")).password;
+		 clickElement(By.xpath("//input[@type='submit']"));
+	}
+
+	public void elementSendKey(By locator, String data) {
+		getElemnet(locator).sendKeys(data);
+	}
+
+	public WebElement getElemnet(By locator) {
+		return driver.findElement(locator);
+	}
 
 }
