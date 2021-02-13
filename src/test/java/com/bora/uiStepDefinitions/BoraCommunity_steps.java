c38e1191c9eaf41da176e9bde11be124c8957058
jayewang <jayewang@localhost>
2021-02-13 15:43:52 -0500

    2/13/2021
    Keyword driven framework

--- src/test/java/com/bora/uiStepDefinitions/BoraCommunity_steps.java
+++ src/test/java/com/bora/uiStepDefinitions/BoraCommunity_steps.java
@@ -10,46 +10,62 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import com.bora.utilities.BoraKeyword_library;
 import com.bora.utilities.DriverFactory;
 import com.bora.utilities.UI_Utils;
 
 public class BoraCommunity_steps {
-	
-	private WebDriver driver = DriverFactory.getInstance();
+
+	private BoraKeyword_library lib = new BoraKeyword_library(DriverFactory.getInstance());
+
+	By editProfileLink = By.xpath("//*[@href=\"/edit-profile\"]");
+	By loginButton = By.linkText("Login");
+	By loginPage_errorMessage = By.xpath("//div[@class='alert alert-danger']");
+
+	String editProfilePageURL = "https://boratech.herokuapp.com/edit-profile";
+	String loginPageURL = "https://boratech.herokuapp.com/login";
 
 	@Given("I'm on the Bora Community homepage")
 	public void i_m_on_the_bora_community_homepage() {
-		driver.get("https://boratech.herokuapp.com/");
+		lib.openUrl("https://boratech.herokuapp.com/");
 	}
 
 	@When("I click on login button")
 	public void i_click_on_login_button() {
-		driver.findElement(By.linkText("Login")).click();
+		lib.clickElement(loginButton);
 	}
 
 	@When("I log in with email {string} and password {string}")
 	public void i_log_in_with_email_and_password(String userEmail, String userPassword) throws InterruptedException {
-		WebElement emailField = driver.findElement(By.name("email"));
-		emailField.sendKeys(userEmail);
-		WebElement passwordField = driver.findElement(By.name("password"));
-		passwordField.sendKeys(userPassword);
-		driver.findElement(By.xpath("//input[@type='submit']")).click();
-		UI_Utils.waitFor(1);
-	}
 
-	@Then("I should still be on the login page")
-	public void i_should_still_be_on_the_login_page() {
-		String actualUrl = driver.getCurrentUrl();
-		String expectedUrl = "https://boratech.herokuapp.com/login";
-		Assert.assertEquals(expectedUrl, actualUrl);
+		lib.login(userEmail, userPassword);
+
+		UI_Utils.waitFor(1);
 	}
 
 	@Then("I will see an error message that says {string}")
 	public void i_will_see_an_error_message_that_says(String expectedAlertText) {
-		WebElement alert = driver.findElement(By.xpath("//div[@class='alert alert-danger']"));
+		WebElement alert = lib.getElement(loginPage_errorMessage);
 		Assert.assertTrue(alert.isDisplayed());
 		String actualAlertText = alert.getText();
 		Assert.assertEquals(expectedAlertText, actualAlertText);
+	}
+
+	@When("I click on Edit Profile link")
+	public void i_click_on_edit_profile_link() {
+		lib.clickElement(editProfileLink);
+	}
+
+	@Then("The edit profile page should display")
+	public void the_edit_profile_page_should_display() {
+
+		Assert.assertTrue("Login page didn't display", lib.compareUrl(editProfilePageURL));
+
+	}
+
+	@Then("I should still be on the login page")
+	public void i_should_still_be_on_the_login_page() {
+		Assert.assertTrue(lib.compareUrl(loginPageURL));
 	}
 
 }
